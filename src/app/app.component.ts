9cd8ff3066d8c29b4c3a2b3d6949d9781055e009
Michael Njuguna <michaelnjuguna184@gmail.com>
2022-08-01 10:21:43 +0300

    added share button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,12 @@ export class AppComponent {
   }
   
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'More apps', url: 'https://play.google.com/store/apps/developer?id=michael+njuguna', icon: 'logo-google-playstore' },
+    { title: 'About app', url: 'https://machakos-university-students-app.netlify.app/', icon: 'globe-sharp' },
+    { title: '+254 735 247 939', url: 'tel:+254 735 247 939', icon: 'Call-sharp' },
+    { title: '+254 795 207 117', url: 'tel:+254 795 207 117', icon: 'call-sharp' },
+    { title: 'Complaints', url: 'tel:0769596233', icon: 'call-sharp' },
+    { title: 'Home', url: 'www.mksu.ac.ke', icon: 'home-sharp' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
