e9f77fbd51394a9b03d83183711ada39a69f383a
Michael Njuguna <michaelnjuguna184@gmail.com>
2022-08-02 07:50:57 +0300

    added buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 //button information
-
+public buttonInfos=[
+  {title:'Portal',icon:'person-circle-sharp',url:'http://portal.mksu.ac.ke/login/sign-in'},
+  {title:'Exam bank',icon:'document-sharp',url:'http://ir.mksu.ac.ke/handle/123456780/187'},
+  {title:'E-Learning LMS',icon:'phone-portrait-sharp',url:'https://elearning.mksu.ac.ke/mksu.lms/'},
+  {title:'University news',icon:'newspaper-sharp',url:'https://www.mksu.ac.ke/blog-medium/'},
+  {title:'Upcoming events',icon:'calendar-sharp',url:'https://www.mksu.ac.ke/upcoming-events/'},
+  {title:'GSSP Fees Structures',icon:'wallet-sharp',url:'https://www.mksu.ac.ke/feesstructures2019/'},
+  {title:'PSSP Fees Structures',icon:'wallet-sharp',url:'https://www.mksu.ac.ke/notices/fee-structures-undergraduate-pssp-students/'},
+  {title:'TVET Fees Structures',icon:'wallet-sharp',url:'https://www.mksu.ac.ke/tvet-fee-structures/'},
+  {title:'Postgraduate Fees Structures',icon:'wallet-sharp',url:'https://www.mksu.ac.ke/postgraduate-fees/'},
+]
   constructor() { }
 
   ngOnInit() {}
