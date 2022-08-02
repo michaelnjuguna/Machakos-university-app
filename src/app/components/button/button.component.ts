a08ba49da84750cebd8012213c00524383341595
Michael Njuguna <michaelnjuguna184@gmail.com>
2022-08-02 06:04:35 +0300

    imported buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+//button information
 
   constructor() { }
 
--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import {ButtonComponent} from '../components/button/button.component'
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { FolderPage } from './folder.page';
     IonicModule,
     FolderPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [FolderPage,ButtonComponent]
 })
 export class FolderPageModule {}
